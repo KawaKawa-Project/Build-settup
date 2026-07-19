de3abafc6f98c0a72c73f535fcd610cbf65d91fb
KawaKawa-Project <dcsnaff@gmail.com>
2026-07-19 15:12:19 +0700

    Refactor Lunaris-Build.sh comments and paths
    Updated comments for clarity and corrected file paths for marble device.

--- Lunaris-Build.sh
+++ Lunaris-Build.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
 
-#initial rom source
+# 1. Initial ROM source
 repo init -u https://github.com/Lunaris-AOSP/android -b 16.2 --git-lfs
 
-#sync rom 
+# 2. Sync ROM 
 /opt/crave/resync.sh
 
-#clone github device tree
+# 3. Clone device tree
+rm -rf device/xiaomi/marble
 git clone https://github.com/KawaKawa-Project/device_xiaomi_marble_by_aosp_pablo.git -b Lunaris-16.2 device/xiaomi/marble
 
-#clone vendor tree and remove default vendor rom
-rm -rf vendor/lineage
-git clone --depth=1 https://github.com/KawaKawa-Project/vendor_xiaomi_marble_by_pablo.git -b 16 vendor/lineage
+# 4. Clone vendor tree 
+rm -rf vendor/xiaomi/marble
+git clone --depth=1 https://github.com/KawaKawa-Project/vendor_xiaomi_marble_by_pablo.git -b 16 vendor/xiaomi/marble
 
-#start the build rom
+# 5. Start the build ROM
 . build/envsetup.sh
+
+# lunch
 lunch lineage_marble-bp4a-userdebug
 m bacon
 
-#upload to gofile
-if [ -f out/target/product/earth/*202607*.zip ]; then
+# 6. Upload to Gofile 
+if [ -f out/target/product/marble/*202607*.zip ]; then
   wget https://raw.githubusercontent.com/lordgaruda/GoFile-Upload/refs/heads/master/upload.sh
-  chmod +x upload.sh ; ./upload.sh out/target/product/earth/*202607*.zip
+  chmod +x upload.sh ; ./upload.sh out/target/product/marble/*202607*.zip
+else
+  echo "zip not found out/target/product/marble/"
 fi
