23d60640078167407259a973da8644c766d2b628
KawaKawa-Project <dcsnaff@gmail.com>
2026-07-19 18:27:59 +0700

    Update build script with new manifest and steps

--- Lunaris-Build.sh
+++ Lunaris-Build.sh
@@ -3,25 +3,21 @@
 # 1. Initial ROM source
 repo init -u https://github.com/Lunaris-AOSP/android -b 16.2 --git-lfs
 
-# 2. Sync ROM 
+# 1,5 add local manifest
+mkdir -p .repo/local_manifests
+curl -s https://raw.githubusercontent.com/KawaKawa-Project/Build-settup/refs/heads/Lunaris-16.2/Lunaris_local_manifest.xml -o .repo/local_manifests/marble.xml
+
+# 2. Sync ROM
 /opt/crave/resync.sh
 
-# 3. Clone device tree
-rm -rf device/xiaomi/marble
-git clone https://github.com/KawaKawa-Project/device_xiaomi_marble_by_aosp_pablo.git -b Lunaris-16.2 device/xiaomi/marble
-
-# 4. Clone vendor tree 
-rm -rf vendor/xiaomi/marble
-git clone --depth=1 https://github.com/KawaKawa-Project/vendor_xiaomi_marble_by_pablo.git -b 16 vendor/xiaomi/marble
-
-# 5. Start the build ROM
+# 3. Start the build ROM
 . build/envsetup.sh
 
 # lunch
 lunch lineage_marble-bp4a-userdebug
 m bacon
 
-# 6. Upload to Gofile 
+# 4. Upload to Gofile 
 if [ -f out/target/product/marble/*202607*.zip ]; then
   wget https://raw.githubusercontent.com/lordgaruda/GoFile-Upload/refs/heads/master/upload.sh
   chmod +x upload.sh ; ./upload.sh out/target/product/marble/*202607*.zip
