adf1e89e8392e790233989c1d63710ac52498007
KawaKawa-Project <dcsnaff@gmail.com>
2026-07-21 05:27:46 +0700

    Clean up local manifests and device directory
    Remove local manifests and device directory before initializing the ROM source and cloning the local manifest.

--- Lunaris-Build.sh
+++ Lunaris-Build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
+rm -rf .repo/local_manifests
+rm -rf device/xiaomi/marble
 # 1. Initial ROM source
 repo init -u https://github.com/Lunaris-AOSP/android -b 16.2 --git-lfs
 
 # 1,5 add local manifest
-rm -rf .repo/local_manifests
-rm -rf device/xiaomi/marble
 git clone https://github.com/KawaKawa-Project/local_manifest.git -b Lunaris_local_manifest .repo/local_manifests
 
 # 2. Sync ROM
