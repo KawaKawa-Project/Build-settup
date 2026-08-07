dc04e0afafd1a4ce16f2221fe53f2531c37ca24b
KawaKawa-Project <dcsnaff@gmail.com>
2026-08-08 05:11:40 +0700

    Add script to initialize and build ROM for Lineage 16.2
    This script initializes the ROM source, adds a local manifest, syncs the ROM, and starts the build process.

--- infinity-x-16.2.sh
+++ infinity-x-16.2.sh
@@ -1,1 +1,21 @@
 
+#!/bin/bash
+
+#Clean
+rm -rf .repo/local_manifests
+
+# 1. Initial ROM source
+repo init -u https://github.com/Lunaris-AOSP/android -b 16.2 --git-lfs --depth=1
+
+# 1,5 add local manifest
+git clone https://github.com/KawaKawa-Project/local_manifest.git -b Lunaris_local_manifest .repo/local_manifests
+
+# 2. Sync ROM
+/opt/crave/resync.sh
+
+# 3. Start the build ROM
+. build/envsetup.sh
+
+# 4. lunch & build
+lunch lineage_marble-bp4a-userdebug
+m bacon
