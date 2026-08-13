f2a64348d796774836cfeb0b14d884358ed6024e
KawaKawa-Project <dcsnaff@gmail.com>
2026-08-14 05:53:27 +0700

    Implement Telegram notifications in build script
    Added Telegram bot notifications for build start and completion.

--- Lunaris-Build-16.2.sh
+++ Lunaris-Build-16.2.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-#Clean
+TOKEN_BOT="8912165324:AAH3r8qs7Mtl1N7_z3W5msuoiyRIZVNfgyg"
+TARGET_SEND="@archivenyamasagung"
+
+START_MSG="New Plan Build Lunaris AOSP%0AMaintainer @SkuyyyLaahhh"
+curl -s -X POST "https://api.telegram.org/bot${TOKEN_BOT}/sendMessage" \
+    -d chat_id="${TARGET_SEND}" \
+    -d text="${START_MSG}"
+
+# Clean
 rm -rf .repo/local_manifests
 
 # 1. Initial ROM source
 repo init -u https://github.com/Lunaris-AOSP/android -b 16.2 --git-lfs --depth=1
 
-# 1,5 add local manifest
+# 1.5 add local manifest
 git clone https://github.com/KawaKawa-Project/local_manifest.git -b Lunaris_local_manifest .repo/local_manifests
 
 # 2. Sync ROM
@@ -20,4 +28,28 @@ lunch lineage_marble-bp4a-userdebug
 m bacon
 
 # 5. pull out build zip
-crave pull out/target/product/*/*zip
+crave pull out/target/product/*/*.zip
+cd marble
+ZIP_NAME=$(ls *.zip | head -n 1)
+
+if [ -f "$ZIP_NAME" ]; then
+    # Github script gofile uploader
+    wget -qO upload.sh https://raw.githubusercontent.com/lordgaruda/GoFile-Upload/refs/heads/master/upload.sh
+    chmod +x upload.sh
+
+    # upload to gofile
+    UPLOAD_OUTPUT=$(./upload.sh "$ZIP_NAME")
+    LINK=$(echo "$UPLOAD_OUTPUT" | grep -Eo 'https://gofile.io/d/[a-zA-Z0-9]+' | head -n 1)
+
+    rm *.zip
+
+    if [ -z "$LINK" ]; then
+        LINK="link not detected"
+    fi
+
+    # Bot Telegram
+    SUCCESS_MSG="New Build Rom By @SkuyyyLaahhh%0A${ZIP_NAME}%0ALink : ${LINK}"
+    curl -s -X POST "https://api.telegram.org/bot${TOKEN_BOT}/sendMessage" \
+        -d chat_id="${TARGET_SEND}" \
+        -d text="${SUCCESS_MSG}"
+fi
