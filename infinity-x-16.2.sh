6672c4f09992d3942529cce19fe8ff1c138d92bd
KawaKawa-Project <dcsnaff@gmail.com>
2026-08-19 07:53:35 +0700

    Update script comments and clean commands

--- infinity-x-16.2.sh
+++ infinity-x-16.2.sh
@@ -2,6 +2,8 @@
 
 #Clean
 rm -rf .repo/local_manifests
+rm -rf kernel/xiaomi/marble
+rm -rf device/xiaomi/marble
 
 # 1. Initial ROM source
 repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
@@ -11,14 +13,16 @@ git clone https://github.com/KawaKawa-Project/local_manifest.git -b infinity-x_l
 
 # 2. Sync ROM
 /opt/crave/resync.sh
+
+# 3. Add Root Resukisu
 cd kernel/xiaomi/marble
 curl -LSs "https://raw.githubusercontent.com/ReSukiSU/ReSukiSU/main/kernel/setup.sh" | bash
 cd ../../../..
 
-# 3. Start the build ROM
+# 4. Start the build ROM
 . build/envsetup.sh
 
-# 4. lunch & build
+# 5. lunch & build
 lunch infinity_marble-userdebug
 make installclean
 m bacon
